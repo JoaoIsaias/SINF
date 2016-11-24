64110eb4f0566485a0b031845860c39ff96a7514
up201305617 <up201305617@fe.up.pt>
2016-11-24 14:58:14 +0000

    wishlist model added

---
+++ OnlineStore/OnlineStore/Lib_Primavera/Model/WishList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstREST.Lib_Primavera.Model
+{
+    public class WishList
+    {
+        public string idCliente
+        {
+            get;
+            set;
+        }
+
+        public string idWishList
+        {
+            get;
+            set;
+        }
+
+        public List<Model.Artigo> Produtos
+        {
+            get;
+            set;
+        }
+    }
+}
